e0b330660f8df82d18a2ee52e65c3937be32ffac
Diogo Castro <diogo.castro@cern.ch>
2020-06-06 20:44:34 +0200

    SWAN theme for MkDocs

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# mkdocs-swan
+# SWAN MkDocs theme
+
+Theme for the [SWAN](https://cern.ch/swan) help and gallery [MkDocs](https://www.mkdocs.org/) webpages, based on [Material for MkDocs](https://squidfunk.github.io/mkdocs-material/).
+
+## How to use
+
+Install the package:
+```
+pip install mkdocs-swan
+```
+
+And add it to `mkdocs.yml`:
+```yml
+theme:
+  name: 'swan'
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# Load README contents
+with open("README.md", encoding = "utf-8") as data:
+    long_description = data.read()
+
+setup(
+    name="mkdocs-swan",
+    version='0.0.1',
+    url='https://github.com/swan-cern/mkdocs-swan',
+    license='BSD',
+    description='SWAN theme for MkDocs',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    author='Diogo Castro',
+    author_email='diogo.castro@cern.ch',
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Text Processing :: Markup :: HTML"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['mkdocs-material==5.*'],
+    entry_points={
+        'mkdocs.themes': [
+            'swan = mkdocs_swan',
+        ]
+    },
+    zip_safe=False
+)
