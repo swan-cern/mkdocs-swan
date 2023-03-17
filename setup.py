c61b358ee76c12f98ea50b3f1b21fe7a8fd7bf10
Diogo Castro <diogo.castro@cern.ch>
2023-03-17 13:57:02 +0100

    v0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding = "utf-8") as data:
 
 setup(
     name="mkdocs-swan",
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/swan-cern/mkdocs-swan',
     license='BSD',
     description='SWAN theme for MkDocs',
